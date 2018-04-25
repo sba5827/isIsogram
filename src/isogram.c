497809bc87b11348dd1ec94e960697ee09a14a91
Steven <sba5827@students.fhv.at>
2018-04-25 12:39:10 +0200

    Is still an isogram if it contains special chars or numbers
    Signed-off-by: Steven <sba5827@students.fhv.at>

--- src/isogram.c
+++ src/isogram.c
@@ -1,19 +1,39 @@
 #include "isogram.h"
 #include <string.h>
 
+#define CASE_DIFF ('a'-'A')
+
+char toUpperCase(const char c){
+    if(c >= 'a' && c <= 'z'){
+        return c - CASE_DIFF;
+    }
+    return c;
+}
+
+bool is_a_letter(const char c){
+    if(c >= 'A' && c <= 'z'){
+        return true;
+    }
+    return false;
+}
+
 bool is_isogram(const char phrase[]){
 
     int occ[256] = {0};
 
     for(int i = 0; i < strlen(phrase); ++i){
-        occ[phrase[i]]++;
+
+        int ascii_index = (int)toUpperCase(phrase[i]);
+
+        if(is_a_letter(ascii_index)){
+            if(occ[ascii_index]++ > 0){
+                return false;
+            }
+        }
+
     }
 
-    for(int i = 0; i < 256; ++i){
-        if(occ[i] > 1){
-            return false;
-        }
-    }
     return true;
+
 
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -19,6 +19,29 @@ void test_word_letter(void){
     TEST_ASSERT_TRUE(is_isogram("isogram"));
 }
 
+void test_cases_letter(void){
+    TEST_ASSERT_FALSE(is_isogram("aA"));
+}
+
+void test_numbers_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("777"));
+}
+
+void test_numbers_and_chars_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("5ab5"));
+    TEST_ASSERT_FALSE(is_isogram("5aa5"));
+}
+
+void test_blanks_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("a b c"));
+    TEST_ASSERT_FALSE(is_isogram("aa bb cc"));
+}
+
+void test_specialChars_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("!!!abc "));
+    TEST_ASSERT_FALSE(is_isogram("!!!aabbcc "));
+}
+
 int main(void)
 {
    UnityBegin("test/test_isogram.c");
@@ -27,6 +50,11 @@ int main(void)
    RUN_TEST(test_single_letter);
    RUN_TEST(test_duplicate_letter);
    RUN_TEST(test_word_letter);
+   RUN_TEST(test_cases_letter);
+   RUN_TEST(test_numbers_letter);
+   RUN_TEST(test_numbers_and_chars_letter);
+   RUN_TEST(test_blanks_letter);
+   RUN_TEST(test_specialChars_letter);
 
    UnityEnd();
    return 0;
