1b1a4f117ab9ab988ddba9a30e80c46201ae1c95
Steven <sba5827@students.fhv.at>
2018-04-25 12:02:19 +0200

    Tests added is_isogram for all usecases programmed
    Signed-off-by: Steven <sba5827@students.fhv.at>

--- src/isogram.c
+++ src/isogram.c
@@ -1,6 +1,19 @@
 #include "isogram.h"
+#include <string.h>
 
 bool is_isogram(const char phrase[]){
 
-  return phrase != 0;
+    int occ[256] = {0};
+
+    for(int i = 0; i < strlen(phrase); ++i){
+        occ[phrase[i]]++;
+    }
+
+    for(int i = 0; i < 256; ++i){
+        if(occ[i] > 1){
+            return false;
+        }
+    }
+    return true;
+
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -7,13 +7,26 @@ void test_empty_string(void)
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
+void test_single_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("a"));
+}
+
+void test_duplicate_letter(void){
+    TEST_ASSERT_FALSE(is_isogram("aa"));
+}
+
+void test_word_letter(void){
+    TEST_ASSERT_TRUE(is_isogram("isogram"));
+}
+
 int main(void)
 {
    UnityBegin("test/test_isogram.c");
 
    RUN_TEST(test_empty_string);
-
-   printf("Test\n");
+   RUN_TEST(test_single_letter);
+   RUN_TEST(test_duplicate_letter);
+   RUN_TEST(test_word_letter);
 
    UnityEnd();
    return 0;
